68157ba44d0ef561af328863c79c3c25d8cd8e5b
Douroh <douglas.frohmann@gmail.com>
2014-11-17 00:55:08 +0200

    Create task10.sql

---
+++ task10.sql
@@ -0,0 +1,2 @@
+INSERT INTO Customer(FirstName, LastName, Company, Country, City, PostalCode, Email) VALUES('Douglas', 'Rohmann', 'BGF', 'Hungary', 'Budapest', '1090', 'Email');
+		    SELECT * FROM Customer WHERE Company='BGF' AND FirstName='Douglas';
